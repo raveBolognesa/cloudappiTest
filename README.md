e0550a33795c200f4d1361ead6b5c458894b029b
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-07-04 21:13:12 +0200

    rafa

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # vueTest
 https://ravebolognesa.github.io/vueTest
+db using jsonPlaceholder http://my-json-server.typicode.com/raveBolognesa/vueTest/db
--- component/message.js
+++ component/message.js
@@ -1,13 +1,22 @@
 Vue.component('message',{
     data(){
         return{
-            message: `Hola rafa`
+            message: `Hola rafa`,
+            info: null,
         }
     },
+    mounted () {
+        axios
+          .get('http://my-json-server.typicode.com/raveBolognesa/vueTest/db')
+          .then(response => (this.info = response))
+      },
     template: `
     <div>
       <h2>Desde componente</h2>
       <p> {{ message }}
+      </p>
+
+      <p> {{ info }}
       </p>
    
       
